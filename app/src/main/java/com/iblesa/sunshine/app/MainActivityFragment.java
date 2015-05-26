349b83f406bcae46ef77b5ac51899facf8c62e8f
Ismael Blesa <iblesa@Osiris.local>
2015-05-26 21:33:45 +0100

    Populating fake data

--- app/src/main/java/com/iblesa/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/iblesa/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +22,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        final View inflate = inflater.inflate(R.layout.fragment_main, container, false);
+        String[] forecastArray = {"Monday - Sunny - 25/13"
+                ,"Tuesday - Sunny - 25/13"
+                ,"Wednesday - Sunny - 25/13"
+                ,"Thursday - Sunny - 25/13"
+                ,"Friday - Sunny - 25/13"
+                ,"Saturday - Sunny - 25/13"
+                ,"Sunday - Sunny - 25/13"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
+        return inflate;
     }
 }
